732ff1f943e6d6c2d4cd52d97d25eb32ad7af4f7
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-06 23:21:47 +0200

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const itemEl = document.createElement('li');
+const itemEl2 = document.createElement('li');
+const itemEl3 = document.createElement('li');
+
+const imageEl = document.createElement('img');
+imageEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl.alt = 'White and Black Long Fur Cat';
+imageEl.width = 340;
+console.log(imageEl);
+
+const imageEl2 = document.createElement('img');
+imageEl2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl2.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+imageEl2.width = 340;
+console.log(imageEl2);
+
+const imageEl3 = document.createElement('img');
+imageEl3.src =  'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl3.alt = 'Group of Horses Running';
+imageEl3.width = 340;
+console.log(imageEl3);
+
+document.body.appendChild(imageEl);
+document.body.appendChild(imageEl2);
+document.body.appendChild(imageEl3);
