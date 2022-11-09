1098837c13fdfc640eab162ec9fac8835017f5ce
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-09 10:09:06 +0200

    add 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const numberCategoriesEl = document.querySelectorAll('li.item');
+console.log('Number of categories:', numberCategoriesEl.length);
+
+const titleEl = document.querySelector('h2');
+const textContentEl = titleEl.textContent;
+console.log('Category:', textContentEl);
+
+const navElement = document.querySelectorAll('ul');
+console.log('Elements:', navElement.length);
