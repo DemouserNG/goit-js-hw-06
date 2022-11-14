97f91f9550c39037dc0a03455ada8dc0caa1e415
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-14 22:05:17 +0200

    delete div

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const div = document.querySelector('.widget');
 const span = document.querySelector('.color');
 const btnChangeColor = document.querySelector('.change-color');
 const body = document.querySelector('body');
