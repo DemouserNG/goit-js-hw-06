d5b1989c8786efcbf3df477fd4f4b3e5d377fd94
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-14 21:46:39 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const div = document.querySelector('.widget');
+const span = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+btnChangeColor.addEventListener('click', onbtnChangeColor);
+
+function onbtnChangeColor(event) {
+  const color = getRandomHexColor();
+  
+  span.textContent = color;
+  body.style.backgroundColor = color;
+
+  console.log(getRandomHexColor());
+
+};
