95a37d65be2b8d6dfeecbf5d3c2571bf5f5b8b0c
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-14 21:12:32 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email.lenght === 0 || password.lenght === 0) {
+        alert('Заповніть поле');
+        return;
+    };
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData);
+
+    form.reset();
+
+};
